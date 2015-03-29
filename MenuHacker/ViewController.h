e56b71cdbcb3c21557d3f85a51b081ed0177d6b9
Will Smith <github@septemberlabs.com>
2015-03-28 22:47:40 -0700

    Initial Commit

---
+++ MenuHacker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MenuHacker
+//
+//  Created by Will Smith on 3/28/15.
+//  Copyright (c) 2015 Will Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MenuHackerTests/MenuHackerTests.m
@@ -0,0 +1,40 @@
+//
+//  MenuHackerTests.m
+//  MenuHackerTests
+//
+//  Created by Will Smith on 3/28/15.
+//  Copyright (c) 2015 Will Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MenuHackerTests : XCTestCase
+
+@end
+
+@implementation MenuHackerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
